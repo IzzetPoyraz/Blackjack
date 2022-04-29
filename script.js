92bb49e0b110093702a987d71339b883174fbd39
Isaak De Coninck <isaakdec@gmail.com>
2022-04-29 09:21:19 +0200

    Classes kaart en kaartboek

---
+++ script.js
@@ -0,0 +1,23 @@
+const SOORT = ['H', 'K', 'R', 'S'];
+const WAARDE = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'B', 'D', 'H', 'A']
+
+class KaartBoek {
+    constructor(kaarten = nieuwDeck()) {
+        this.kaarten = kaarten;
+    }
+}
+
+class Kaart {
+    constructor(kaartSoort, kaartWaarde) {
+        this.kaartSoort = soort;
+        this.kaartWaarde = waarde;
+    }
+}
+
+function nieuwDeck() {
+    return SOORT.flatMap(soort => {
+        return VALUES.map(waarde => {
+            return new Kaart(soort, waarde)
+        })
+    })
+}
