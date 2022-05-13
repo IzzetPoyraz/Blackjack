b89a94fa46da7913e6d21aaf3987f87656dec7c7
Jonas Van Kerkhove <Jonas0072003@outlook.com>
2022-05-13 12:00:24 +0200

    betten

---
+++ chips.js
@@ -0,0 +1,49 @@
+'use strickt'
+
+const een = document.querySelector('#1punten')
+const vijf = document.querySelector('#5punten')
+const vijventwintig = document.querySelector('#25punten')
+const fijftig = document.querySelector('#50punten')
+const honderd = document.querySelector('100punten')
+const vijfhonderd = document.querySelector('500punten')
+const duizend = document.querySelector('1000punten')
+
+let betAmount = 0;
+let chipsTotaal = 1000;
+
+function check (value){
+    if (value>chipsTotaal){
+        return false
+    } else {
+        return true
+    }
+}
+
+function bet (value){
+    if (check(value)){
+        betAmount+=value
+        chipsTotaal-=value
+    }
+}
+
+een.addEventListener("click", () =>{
+    bet(1)
+})
+vijf.addEventListener("click", () =>{
+    bet(5)
+})
+vijventwintig.addEventListener("click", () =>{
+    bet(25)
+})
+fijftig.addEventListener("click", () =>{
+    bet(50)
+})
+honderd.addEventListener("click", () =>{
+    bet(100)
+})
+vijfhonderd.addEventListener("click", () =>{
+    bet(500)
+})
+duizend.addEventListener("click", () =>{
+    bet(1000)
+})
