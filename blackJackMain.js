b940e7763eb514be910ac6ffce278bdf5e3691ac
runeleemans <rune.leemans@student.odisee.be>
2022-05-19 19:58:34 +0200

    js

--- blackJackMain.js
+++ blackJackMain.js
@@ -1,1 +1,13 @@
+const split = document.querySelector("#split");
+const hit = document.querySelector("#hit");
+const stand = document.querySelector("#stand");
+const double = document.querySelector("#double");
+
+const p1Kaart = docuement.querySelector(`.player1PlaceHolder > span:nth-child(${kaartIncex})`);
+hit.addEventListener('click', play);
+stand.addEventListener('click', () => {
+    playerStand = 1;
+    play();
+});
+spilt('click', split());
 
