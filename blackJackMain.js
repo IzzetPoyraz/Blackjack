2002f42a0ef24a94df3e9608a4118f42263a8c67
Isaak De Coninck <isaakdec@gmail.com>
2022-06-08 20:08:02 +0200

    update blackJackMain to normal functions

--- blackJackMain.js
+++ blackJackMain.js
@@ -20,98 +20,172 @@
 // });
 
 // spilt('click', split());
+
+
+
 import { deck } from "./kaartBoek.js";
 
-class blackJack {
-    firstDraw = true;
-    constructor() {
-        this.deck = deck.kaarten;
-    }
-    startGame() {
-        let dealer = new Dealer();
-        let player = new Player();
-    
-    }
-    // neem kaarten van het geshuffelde deck
-    drawCards() {
-        let hand = [];
+// class blackJack {
+//     firstDraw = true;
+//     constructor() {
+//         this.deck = deck.kaarten;
+//     }
+//     startGame() {
+//         const overlay = document.querySelector('.overlayStart');
+//         overlay.style.display = 'none';
+//     }
+//     // neem kaarten van het geshuffelde deck
+//     drawCards() {
+//         let hand = [];
         
-        for (let i = 0 ; i < 2 ; i++) {
-            let randomkaart = this.deck.shift();
-            hand.push(randomkaart);
-        }
+//         for (let i = 0 ; i < 2 ; i++) {
+//             let randomkaart = this.deck.shift();
+//             hand.push(randomkaart);
+//         }
         
-        return hand;
-    }
+//         return hand;
+//     }
 
-    drawCard(hand) {
-        let randomkaart = this.deck.shift();
-        hand.push(randomkaart);
+//     drawCard(hand) {
+//         let randomkaart = this.deck.shift();
+//         hand.push(randomkaart);
 
-        return hand;
-    }
+//         return hand;
+//     }
 
-    checkSplit() {
-        if(player.hand[0].spelWaarde === player.hand[1].spelWaarde){
-            split.classList.toggle('display');
-            return true;
-        }
-        else{
-            false;
-        }
-    }
-    spilt(){
-        player.splitHand = player.hand.pop;
-        split.classList.toggle('display');
-    }
-}
+//     checkSplit() {
+//         if(player.hand[0].spelWaarde === player.hand[1].spelWaarde){
+//             split.classList.toggle('display');
+//             return true;
+//         }
+//         else{
+//             false;
+//         }
+//     }
+//     spilt(){
+//         player.splitHand = player.hand.pop;
+//         split.classList.toggle('display');
+//     }
+// }
 
 
-class Person extends blackJack {
-    hand = [];
-    totaleKaartWaarde;
+// class Person extends blackJack {
+//     hand = [];
+//     totaleKaartWaarde;
     
-    get cardValue() {
-        this.totaleKaartWaarde = 0;
-        for (let i = 0 ; i < this.hand.length ; i++) {
-            this.totaleKaartWaarde += this.hand[i].spelWaarde;
-        }
-        return this.totaleKaartWaarde;
-    }
+//     get cardValue() {
+//         this.totaleKaartWaarde = 0;
+//         for (let i = 0 ; i < this.hand.length ; i++) {
+//             this.totaleKaartWaarde += this.hand[i].spelWaarde;
+//         }
+//         return this.totaleKaartWaarde;
+//     }
 
-    stand() {
+//     stand() {
 
-    }
+//     }
 
-}
+// }
 
-class Dealer extends Person{
+// class Dealer extends Person{
 
-}
+// }
 
-class Player extends Person{
-    splitHand = [];
-    canSplit = false;
-}
+// class Player extends Person{
+//     splitHand = [];
+//     canSplit = false;
+// }
+
+// const game = new blackJack();
+// const player = new Player();
+// const dealer = new Dealer();
+// const overlay = document.querySelector('.overlayStart');
+// console.log(overlay);
+// const startButton = document.getElementById('buttonPlay');
+// startButton.addEventListener('click', game.startGame());
 
 // Start game
 // Maak objecten aan
-const game = new blackJack();
-const dealer = new Dealer();
-const speler = new Player();
 
-// Deal elke speler en dealer eerste twee kaarten
-speler.hand = speler.drawCards();
-dealer.hand = dealer.drawCards();
+// console.log(game.deck);
+// console.log(player.hand);
+// console.log(dealer.hand);
 
-console.log(game.deck);
-console.log(speler.hand);
-console.log(dealer.hand);
+// console.log(player.cardValue);
 
-console.log(speler.cardValue);
+// player.drawCard(player.hand);
+// console.log(player.hand);
+// console.log(player.cardValue);
 
-speler.drawCard(speler.hand);
-console.log(speler.hand);
-console.log(speler.cardValue);
+
+
+
+
+
+
+
+// Init Variables
+
+let playerHand = [], dealerHand = [];
+let playerHandValue = 0, dealerHandValue = 0, playerSplitHandValue = 0;
+let playerCanSplit = false;
+let gameDeck = deck.kaarten;
+
+//Select and control DOMelements
+
+const startButton = document.getElementById('buttonPlay');
+const overlay = document.querySelector('.overlayStart');
+const coloredOverlay = document.querySelector('.overlay');
+console.log(overlay)
+startButton.addEventListener('click', function () {
+    overlay.style.display = 'none';
+    coloredOverlay.style.display = 'none';
+});
+
+// Game Functions
+
+function drawCards(hand, handValue) {
+            
+    for (let i = 0 ; i < 2 ; i++) {
+        let randomkaart = gameDeck.shift();
+        hand.push(randomkaart);
+    }
+
+    handValue = hand.forEach(card => {
+        handValue += card.spelWaarde;
+    });
+    
+}
+
+function drawCard(hand) {
+    let randomkaart = this.deck.shift();
+    hand.push(randomkaart);
+
+    
+
+}
+
+function checkSplit() {
+    if (player.hand[0].spelWaarde === player.hand[1].spelWaarde) {
+        playerCanSplit = true;
+
+    }
+}
+
+// Game
+
+console.log(gameDeck)
+
+drawCards(playerHand, playerHandValue);
+console.log(playerHandValue);
+drawCards(dealerHand, dealerHandValue);
+console.log(dealerHandValue)
+
+
+console.log(playerHand);
+console.log(dealerHand);
+
+
+
 
 
